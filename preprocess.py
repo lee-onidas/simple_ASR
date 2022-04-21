6d2ec7cc7e37b69eb9f88a6950a275d79e5feadd
Lee Steven <l.stevennz@gmail.com>
2022-04-21 15:07:51 +1200

    implemented get_lables, wav2mfcc, and save_data_to_array functions

---
+++ preprocess.py
@@ -0,0 +1,52 @@
+import librosa
+import os
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+from tqdm import tqdm
+
+DATA_PATH = "./data/"
+
+
+# Input: Folder Path
+# Output: Tuple (Label, Indices of the labels, one-hot encoded labels)
+def get_labels(path=DATA_PATH):
+    labels = os.listdir(path)
+    label_indices = np.arange(0, len(labels))
+    return labels, label_indices, to_categorical(label_indices)
+
+
+# convert file to wav2mfcc
+# Mel-frequency cepstral coefficients
+def wav2mfcc(file_path, n_mfcc=20, max_len=11):
+    wave, sr = librosa.load(file_path, mono=True, sr=None)
+    #downsampling
+    wave = wave[::3]
+    mfcc = librosa.feature.mfcc(y=wave, sr=16000, n_mfcc=n_mfcc)
+
+    # If maximum length exceeds mfcc lengths then pad the remaining ones
+    if (max_len > mfcc.shape[1]):
+        pad_width = max_len - mfcc.shape[1]
+        mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)), mode='constant')
+
+    # Else cutoff the remaining parts
+    else:
+        mfcc = mfcc[:, :max_len]
+
+    return mfcc
+
+
+# Input: Folder path, maximum length, number of mfccs to extract
+# Output: numpy array file "*.npy" for each label
+def save_data_to_array(path=DATA_PATH, max_len=11, n_mfcc=20):
+    labels, _, _ = get_labels(path)
+
+    for label in labels:
+        # Init mfcc vectors
+        mfcc_vectors = []
+
+        wavfiles = [path + label + '/' + wavfile for wavfile in os.listdir(path + '/' + label)]
+        for wavfile in tqdm(wavfiles, "Saving vectors of label - '{}'".format(label)):
+            mfcc = wav2mfcc(wavfile, max_len=max_len, n_mfcc=n_mfcc)
+            mfcc_vectors.append(mfcc)
+        np.save('./processed_data/' + label + '.npy', mfcc_vectors)
